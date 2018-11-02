b2217e9e0224f871bb09ad3b6f760f6be153a556
Jonathan Ribas <jonathan.ribas@fraudbuster.mobi>
2018-11-02 16:48:21 +0100

    Use markdown to generate articles

--- Makefile
+++ Makefile
@@ -1,9 +1,12 @@
+INSTALL_DIR	=	~/public_html/fastcgi/
+
 NAME		=	myblog.cgi
 SRC			=	myblog.c
 OBJ			=	$(SRC:.c=.o)
 
 CSS					=	my.css
 ARTICLES_MD			=	$(sort $(wildcard  *.md))
+ARTICLES_HTML		=	$(ARTICLES_MD:.md=.html)
 
 CFLAGS	=
 #LDFLAGS =	-lfcgi
@@ -12,9 +15,12 @@ RM		=	rm -f
 CC		=	gcc
 
 %.o		:	%.c
-			$(CC) -o $@ -c $< $(CFLAGS) -DARTICLES="$(ARTICLES_MD)" -DCSS="$(CSS)"
+			$(CC) -o $@ -c $< $(CFLAGS) -DARTICLES="$(ARTICLES_HTML)" -DCSS="$(CSS)"
 
-all 	:	cgi
+%.html	:	%.md
+			markdown $< > $@
+
+all 	:	htmls cgi
 
 cgi		:	$(OBJ)
 			$(CC) -o $(NAME) $(OBJ) $(CFLAGS) $(LDFLAGS)
@@ -28,3 +34,7 @@ fclean 	:	clean
 			$(RM) $(NAME) $(ARTICLES_HTML)
 
 re		:	fclean all
+
+install	:	all
+			mkdir -p $(INSTALL_DIR)
+			cp -f $(NAME) $(ARTICLES_HTML) $(CSS) $(INSTALL_DIR)
--- myblog.c
+++ myblog.c
@@ -23,38 +23,6 @@ int dump_content(const char* a)
     return (0);
 }
 
-#define H1 "# "
-#define H2 "## "
-#define H3 "### "
-#define QUOTE "> "
-#define BULLET "* "
-int dump_article_form_md(const char* a)
-{
-    FILE*   f;
-    char    buf[BUFSZ + 1];
-    char*   s;
-
-    buf[BUFSZ] = '\0';
-    f = fopen(a, "r");
-    if (!f) return (ENOENT);
-    while (fgets(buf, BUFSZ, f)) {
-        if (s = strstr(buf, H3))
-            printf("<h3>%s</h3>", s + strlen(H3));
-        else if (s = strstr(buf, H2))
-            printf("<h2>%s</h2>", s + strlen(H2));
-        else if (s = strstr(buf, H1))
-            printf("<h1>%s</h1>", s + strlen(H1));
-        else if (s = strstr(buf, QUOTE))
-            printf("<blockquote><p>%s</p></blockquote>", s + strlen(QUOTE));
-        else if (s = strstr(buf, BULLET))
-            printf("<ul><li>%s</li></ul>", s + strlen(BULLET));
-        else
-            printf("<p>%s</p>", buf);
-    }
-    fclose(f);
-    return (0);
-}
-
 int main(int ac, char** av)
 {
     char* articles;
@@ -66,7 +34,7 @@ int main(int ac, char** av)
     for (article = strtok(articles, " ");
          article;
          article = strtok(NULL, " ")) {
-        dump_article_form_md(article);
+        dump_content(article);
     }
     free(articles);
     return (0);
